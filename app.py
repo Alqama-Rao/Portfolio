8c0e01bf90aab616327bee6c16499c53577c9542
Alqama-Rao <alqamarao89@gmail.com>
2023-03-29 04:28:51 +0500

    your commit message here

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
-
 
 @app.route("/api", methods=["GET"])
 def Default():
     return "Hello Deer"
 
-
 @app.route("/api/save-location", methods=["POST"])
 def save_location():
-    print("Save location called")
+    app.logger.debug("Save location called")
     data = request.get_json()
 
-    print("Reading file data")
+    app.logger.debug("Reading file data")
     # Load previous data from file
     try:
         with open("/app/locations.txt", "r") as f:
@@ -24,17 +23,14 @@ def save_location():
     # Add new data to list
     locations.append([data["latitude"], data["longitude"]])
 
-    print("Writing data in file")
-
+    app.logger.debug("Writing data in file")
     # Write data to file
     with open("/app/locations.txt", "w") as f:
         for loc in locations:
             f.write(f"{loc[0]},{loc[1]}\n")
-    import os
-    print(os.getcwd())
 
+    app.logger.debug(f"Current working directory: {os.getcwd()}")
     return jsonify(success=True)
-
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
