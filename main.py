f4d5c59b777ce7574c542d06a97d3927dff6f325
Alqama-Rao <alqamarao89@gmail.com>
2023-03-28 22:33:19 +0500

    your commit message here

--- main.py
+++ main.py
@@ -1,48 +1,37 @@
-# from fastapi import FastAPI, Request
-#
-# app = FastAPI()
-#
-# locations = []
-#
-# @app.post("/api/save-location")
-# async def save_location(request: Request):
-#     data = await request.json()
-#     locations.append(data)
-#     with open("locations.txt", "a") as f:
-#         f.write(f"{data['latitude']},{data['longitude']}\n")
-#     return {"success": True}
-#
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 
+creds = Credentials.from_authorized_user_info(info={
+    'client_id': '303785721663-2t9h9rvhkeoos7kidrlugshbhff2ji20.apps.googleusercontent.com',
+    'client_secret': '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',
+})
 
+# Define the spreadsheet ID and range to write data to
+SPREADSHEET_ID = '1SEw29nHHIlDq149QW78HVtx5rzJCr-FRQn49HzntpOs'
+RANGE_NAME = 'Sheet1!A1:B'
+
+def write_to_sheet(values):
+    service = build('sheets', 'v4', credentials=creds)
+    body = {
+        'values': [values]
+    }
+    result = service.spreadsheets().values().append(
+        spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME,
+        valueInputOption='RAW', insertDataOption='INSERT_ROWS',
+        body=body).execute()
+    print('{0} cells appended.'.format(result \
+            .get('updates') \
+            .get('updatedCells')))
 
 from flask import Flask, request, jsonify
-import csv
 
 app = Flask(__name__)
 
-
-
 @app.route("/api/save-location", methods=["POST"])
 def save_location():
-
     data = request.get_json()
-
-    # Read previous data from file
-    locations = []
-    with open("locations.csv", "r") as f:
-        reader = csv.reader(f)
-        for row in reader:
-            locations.append({"latitude": float(row[0]), "longitude": float(row[1])})
-
-    locations.append(data)
-    with open("locations.csv", "a") as f:
-        f.write(f"{data['latitude']},{data['longitude']}\n")
-    with open("locations.csv", "a", newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([data['latitude'], data['longitude']])
+    values = [data['latitude'], data['longitude']]
+    write_to_sheet(values)
     return jsonify(success=True)
 
 if __name__ == "__main__":
