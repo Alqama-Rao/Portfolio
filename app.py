2299b96f057b245122a644871ce9a93be6da0cfa
Alqama-Rao <alqamarao89@gmail.com>
2023-03-29 04:57:23 +0500

    your commit message here

--- app.py
+++ app.py
@@ -4,19 +4,21 @@ import logging
 
 app = Flask(__name__)
 
+# create a log file in the root directory
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
 @app.route("/api", methods=["GET"])
 def Default():
     return "Hello Deer"
 
 @app.route("/api/save-location", methods=["POST"])
 def save_location():
-    app.logger.setLevel(logging.INFO)
+    logging.info("Request received to save location")
     data = request.get_json()
 
-    app.logger.setLevel(logging.INFO)
     # Load previous data from file
     try:
-        with open("/app/locations.txt", "r") as f:
+        with open("locations.txt", "r") as f:
             locations = [line.strip().split(",") for line in f.readlines()]
     except FileNotFoundError:
         locations = []
@@ -24,13 +26,12 @@ def save_location():
     # Add new data to list
     locations.append([data["latitude"], data["longitude"]])
 
-    app.logger.setLevel(logging.INFO)
     # Write data to file
-    with open("/app/locations.txt", "w") as f:
+    with open("locations.txt", "w") as f:
         for loc in locations:
             f.write(f"{loc[0]},{loc[1]}\n")
 
-    app.logger.debug(f"Current working directory: {os.getcwd()}")
+    logging.debug(f"Current working directory: {os.getcwd()}")
     return jsonify(success=True)
 
 if __name__ == "__main__":
