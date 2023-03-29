244d771ec49cca78ab9cd5b0ec2959cf2c997173
Alqama-Rao <alqamarao89@gmail.com>
2023-03-29 05:03:04 +0500

    your commit message here

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, jsonify
-import os
 import logging
+import os
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-# create a log file in the root directory
-logging.basicConfig(filename='app.log', level=logging.DEBUG)
+app_logger = logging.getLogger(__name__)
+app_logger.setLevel(logging.DEBUG)
 
 @app.route("/api", methods=["GET"])
 def Default():
@@ -13,12 +12,12 @@ def Default():
 
 @app.route("/api/save-location", methods=["POST"])
 def save_location():
-    logging.info("Request received to save location")
+    app_logger.info("Received request to save location")
     data = request.get_json()
 
     # Load previous data from file
     try:
-        with open("locations.txt", "r") as f:
+        with open("/app/locations.txt", "r") as f:
             locations = [line.strip().split(",") for line in f.readlines()]
     except FileNotFoundError:
         locations = []
@@ -27,11 +26,12 @@ def save_location():
     locations.append([data["latitude"], data["longitude"]])
 
     # Write data to file
-    with open("locations.txt", "w") as f:
+    with open("/app/locations.txt", "w") as f:
         for loc in locations:
             f.write(f"{loc[0]},{loc[1]}\n")
 
-    logging.debug(f"Current working directory: {os.getcwd()}")
+    app_logger.debug(f"Current working directory: {os.getcwd()}")
+    app_logger.info("Successfully saved location")
     return jsonify(success=True)
 
 if __name__ == "__main__":
